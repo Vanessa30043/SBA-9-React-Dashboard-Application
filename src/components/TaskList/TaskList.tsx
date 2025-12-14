19afa408aed98697f1425782a57a2153a03e2d96
Vanessa Sierra <sierrava212@yahoo.com>
2025-12-13 23:40:04 -0500

    edited TaskList.tsx file

--- src/components/TaskList/TaskList.tsx
+++ src/components/TaskList/TaskList.tsx
@@ -1,36 +1,37 @@
-// src/components/TaskList/TaskList.tsx
 import React from "react";
 import type { Task } from "../../types";
 import { TaskItem } from "./TaskItem";
 
-type Props = {
+interface TaskListProps {
   tasks: Task[];
   onToggleStatus: (id: string) => void;
   onDelete: (id: string) => void;
   onEdit: (task: Task) => void;
-};
+}
 
-export const TaskList: React.FC<Props> = ({
+export const TaskList: React.FC<TaskListProps> = ({
   tasks,
   onToggleStatus,
   onDelete,
   onEdit,
 }) => {
   if (tasks.length === 0) {
-    return <div style={{ padding: 12 }}>No tasks to show.</div>;
+    return <p>No tasks found.</p>;
   }
 
   return (
-    <div>
-      {tasks.map((t) => (
-        <TaskItem
-          key={t.id}
-          task={t}
-          onToggleStatus={onToggleStatus}
-          onDelete={onDelete}
-          onEdit={onEdit}
-        />
-      ))}
-    </div>
+    <ul style={{ listStyle: "none", padding: 0 }}>
+      {tasks.map((task) => {
+        return (
+          <TaskItem
+            key={task.id}
+            task={task}
+            onToggleStatus={onToggleStatus}
+            onDelete={onDelete}
+            onEdit={onEdit}
+          />
+        );
+      })}
+    </ul>
   );
 };
